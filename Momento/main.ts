7ffc8074ea9f4d12f36f37f93ae3be6c7e749b88
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-08 13:04:56 +0300

    use typescript

--- Momento/main.ts
+++ Momento/main.ts
@@ -1,15 +1,24 @@
 import { Editor } from './Editor.js';
 import { History } from './History.js';
+
+
+
 let editor = new Editor('Hi');
 let history = new History();
+
 editor.setContent('hello');
 editor.setContent('#%#klkl');
 history.push(editor.createState());
+
 editor.setContent('hello world!');
 history.push(editor.createState());
+
+
 editor.setContent('hello world!!');
 console.log(editor.getContent());
+
 editor.restore(history.pop());
 console.log(editor.getContent());
+
 editor.restore(history.pop());
 console.log(editor.getContent());
